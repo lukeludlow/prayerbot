fea0a805af13e48db5ea4149d2de7622bf7f4e0d
lukeludlow <lukeludlow@live.com>
2018-04-14 14:12:09 -0600

    more strings

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ INTRO = [
     "dear father in heaven, ",
     "oh god, the eternal father, ",
     "dear heavenly father, ",
-    "heavenly father, "
+    "heavenly father, ",
+    "our father in heaven, "
+]
+
+PURPOSE = [
+    "we are so blessed to be gathered here as a family. ",
+    "we come together this day to pray. "
 ]
 
 THANKFUL = [
     "we thank thee that we can gather here together today. ",
     "we thank thee for our many blessings. ",
     "we thank thee for all that you have given us. ",
-    "we are grateful for the beautiful weather you have given us. "
+    "we are grateful for the beautiful weather you have given us. ",
+    "we thank thee for the scriptures and the guidance they give us. ",
+    "we thank thee for our prophet and apostles. "
 ]
 
+THANKFUL_FOOD = [
+    "we thank thee for the delicious meal before us. ",
+    "we are grateful for all who worked to provide us this meal. "
+]
 
 ASK = [
     "we ask that we can be blessed with your spirit. ",
@@ -23,11 +35,24 @@ ASK = [
     "please bless us to carry your spirit with us. "
 ]
 
+ASK_FOOD = [
+    "please bless this food. ",
+    "please bless this food to strengthen and nourish our bodies. ",
+    "please bless this food to be nourishing to our bodies and strengthening to our minds. ",
+    "please bless this food to make us healthy and strong. "
+]
+
 
 CLOSING = [
     "we say these things in the name of thy son, jesus christ, amen. ",
-    "in the name of jesus christ, amen. "
+    "in the name of jesus christ, amen. ",
+    "we humbly beseech these blessings, oh father, in the name of thy only begotten son, jesus christ, amen."
 ]
+
+
+SACRAMENT_PRAYER = """
+O God, the Eternal Father, we ask thee in the name of thy Son, Jesus Christ, to bless and sanctify this bread to the souls of all those who partake of it; that they may eat in remembrance of the body of thy Son, and witness unto thee, O God, the Eternal Father, that they are willing to take upon them the name of thy Son, and always remember him, and keep his commandments which he hath given them, that they may always have his Spirit to be with them. Amen. 
+"""
 
 
 
@@ -53,21 +78,27 @@ def on_intent (request, session):
     #called on intent
 
     #intent = request['intent']
-    intent_name = request['intent']['name']
+    intent = request['intent']['name']
 
-    if intent_name == 'BasicPrayer':
-
+    if intent == 'BasicPrayer':
         output = random.choice(INTRO) + "".join(random.sample(THANKFUL, 2)) + "".join(random.sample(ASK, 2)) + random.choice(CLOSING)
         return build_response(output)
 
 
-    elif intent_name == 'DinnerPrayer':
+    elif intent == 'DinnerPrayer':
         output = INTRO + THANKFUL + "please bless the food to strengthen and nourish our bodies." + CLOSING
         return build_response(output)
 
-    elif intent_name == 'RefreshmentPrayer':
+
+    elif intent == 'RefreshmentPrayer':
         output = INTRO + THANKFUL + "please bless these doughnuts and cookies to strengthen and nourish our bodies." + CLOSING
         return build_response(output)
+
+    elif intent == 'SacramentPrayer':
+        return build_response(SACRAMENT_PRAYER)
+
+    elif intent == "AMAZON.StopIntent":
+        return on_session_ended()
 
     else:
         return build_response('error, unknown intent')
