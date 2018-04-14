53341290c0b7c6675032610051dca80a8704e79d
lukeludlow <lukeludlow@live.com>
2018-04-14 16:55:51 -0600

    added more options and more data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 
+# --- populate arrays with prayer phrases --- #
 
 INTRO = [
     "dear father in heaven, ",
@@ -29,6 +30,8 @@ THANKFUL_FOOD = [
     "we are grateful for all who worked to provide us this meal. "
 ]
 
+REFRESHMENTS = "please bless these doughnuts and cookies and stuff to strengthen and nourish our bodies. "
+
 ASK = [
     "we ask that we can be blessed with your spirit. ",
     "please bless us to be kind and loving. ",
@@ -50,14 +53,34 @@ CLOSING = [
 ]
 
 
-SACRAMENT_PRAYER = """
-O God, the Eternal Father, we ask thee in the name of thy Son, Jesus Christ, to bless and sanctify this bread to the souls of all those who partake of it; that they may eat in remembrance of the body of thy Son, and witness unto thee, O God, the Eternal Father, that they are willing to take upon them the name of thy Son, and always remember him, and keep his commandments which he hath given them, that they may always have his Spirit to be with them. Amen. 
-"""
+IDEAS = [
+    "ask to protect the missionaries, ",
+    "ask to have the spirit in your house, ",
+    "ask to be led to the right choices, ",
+    "express gratitude for your blessings, ",
+    "be thankful for your health and safety, ",
+    "express gratitude for your loved ones, ",
+    "ask how to help your loved ones, ",
+    "ask how to serve more, ",
+    "bless the refreshments, ",
+    "ask for heavenly father's guidance, "
+]
+
+# kind of an easter egg - a rote prayer from Alma 31:15
+LONG_PRAYER = (
+    'Holy, holy God; we believe that thou art God, and we believe that thou art holy, and that thou wast a spirit, and that thou '
+    'art a spirit, and that thou wilt be a spirit forever. Holy God, we believe that thou hast separated us from our brethren; '
+    'and we do not believe in the tradition of our brethren, which was handed down to them by the childishness of their fathers; '
+    'but we believe that thou hast elected us to be thy holy children; and also thou hast made it known unto us that there shall be '
+    'no Christ. But thou art the same yesterday, today, and forever; and thou hast elected us that we shall be saved, whilst all '
+    'around us are elected to be cast by thy wrath down to hell; for the which holiness, O God, we thank thee; and we also thank '
+    'thee that thou hast elected us, that we may not be led away after the foolish traditions of our brethren, which doth bind them '
+    'down to a belief of Christ, which doth lead their hearts to wander far from thee, our God. And again we thank thee, O God, that '
+    'we are a chosen and a holy people. Amen. '
+)
 
 
-
-
-# --- entry point --- 
+# --- entry point --- #
 
 def lambda_handler (event, context):
 
@@ -72,30 +95,30 @@ def lambda_handler (event, context):
 
 
 
-# --- response handlers ---
+# --- response handlers --- #
 
 def on_intent (request, session):
-    #called on intent
 
-    #intent = request['intent']
     intent = request['intent']['name']
 
     if intent == 'BasicPrayer':
         output = random.choice(INTRO) + "".join(random.sample(THANKFUL, 2)) + "".join(random.sample(ASK, 2)) + random.choice(CLOSING)
         return build_response(output)
 
-
     elif intent == 'DinnerPrayer':
-        output = INTRO + THANKFUL + "please bless the food to strengthen and nourish our bodies." + CLOSING
+        output = random.choice(INTRO) + random.choice(THANKFUL) + random.choice(THANKFUL_FOOD) + random.choice(ASK) + random.choice(ASK_FOOD) + random.choice(CLOSING)
         return build_response(output)
-
 
     elif intent == 'RefreshmentPrayer':
-        output = INTRO + THANKFUL + "please bless these doughnuts and cookies to strengthen and nourish our bodies." + CLOSING
+        output = random.choice(INTRO) + REFRESHMENTS + random.choice(CLOSING)
         return build_response(output)
 
-    elif intent == 'SacramentPrayer':
-        return build_response(SACRAMENT_PRAYER)
+    elif intent == 'PrayerIdeas':
+        output = "You could " + "or ".join(random.sample(IDEAS, 3))
+        return build_response(output)
+
+    elif intent == 'LongPrayer':
+        return build_response(LONG_PRAYER)
 
     elif intent == "AMAZON.StopIntent":
         return on_session_ended()
@@ -123,10 +146,6 @@ def on_session_ended():
     output = "amen. hallelujah."
 
     return build_response(output)
-
-
-
-
 
 
 
